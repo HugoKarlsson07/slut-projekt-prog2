b36f7fba8c627bebef9d1eaa294eea553c812337
HugoKarlsson07 <hugooscark@gmail.com>
2026-01-19 13:34:38 +0100

    pusha fel

--- 1 - example_requests/lib/request.rb
+++ 1 - example_requests/lib/request.rb
@@ -1,22 +1,47 @@
-
+require 'debug'
 
 #ska retunera method resource version headers params
 class Request
   attr_reader :method, :resource, :version, :headers, :params
   def initialize(fileContent)
-   
+    @params = Hash.new
     @content = fileContent
+
+    @content = @content.split(/\n\n/)
+    param = @content[1]
+
+    @content.delete_at(1)
+    @content = @content[0]
     @content = @content.split(/\r?\n/)
 
-    #p(@content)
+    @method, @resource, @version = @content[0].split(" ")
+    @content.delete_at(0)
+    @headers = Hash.new
 
-    row1, row2, row3, row4 = @content
-    @method, @resource, @version = row1.split(" ")
+  
+    @content.each do |header|
+        if header.include?(": ")
+          header = header.split(": ")
+          @headers[header[0]] = header[1]
+        end
+    end
 
-    host, hoster = row2.split(": ")
-    acc_lang, lang = row3.split(": ")
-    @headers = {host => hoster, acc_lang => lang}
-    @params = row4 = {}
-
+    if @method == "GET"
+      holder = @resource
+      holder = holder.split("?")
+      holder.delete_at(0)
+      holder = holder[0]
+      holder = holder.split("&")
+      holder.each do |param|
+        param = param.split("=")
+        @params[param[0]] = param[1]
+      end
+    elsif @method == "POST"
+      param = param.split("&")
+      param.each do |arg|
+        arg = arg.split("=")
+        @params [arg[0]] = arg[1]
+      end
+    end
   end
 end
--- 1 - example_requests/test/request_test.rb
+++ 1 - example_requests/test/request_test.rb
@@ -3,18 +3,67 @@ require_relative '../lib/request'
 
 class RequestTest < Minitest::Test
 
-  def test_parses_http_method_from_simple_get
-    request_string = File.read('../get-index.request.txt')
+  # def test_parses_http_method_from_simple_get
+  #   request_string = File.read('../get-index.request.txt')
+  #   request = Request.new(request_string)
+
+  #   assert_equal 'GET', request.method
+  # end
+
+  # def test_parses_resource_from_simple_get
+  #   request_string = File.read('../get-index.request.txt')
+  #   request = Request.new(request_string)
+
+  #   assert_equal '/', request.resource
+  # end
+
+   def test_parses_version_from_simple_get
+     request_string = File.read('../get-index.request.txt')
+     request = Request.new(request_string)
+
+     assert_equal 'HTTP/1.1', request.version
+
+   end
+
+  def test_parses_http_method_from_simple_get_request
+    request_string = File.read('../get-fruits-with-filter.request.txt')
     request = Request.new(request_string)
 
     assert_equal 'GET', request.method
   end
 
-  def test_parses_resource_from_simple_get
-    request_string = File.read('../get-index.request.txt')
+  def test_parses_http_resource_from_simple_get_request
+    request_string = File.read('../get-fruits-with-filter.request.txt')
     request = Request.new(request_string)
 
-    assert_equal '/', request.resource
+    assert_equal '/fruits?type=bananas&minrating=4', request.resource
   end
+
+   def test_parses_http_version_from_simple_get_request
+     request_string = File.read('../get-fruits-with-filter.request.txt')
+     request = Request.new(request_string)
+
+    assert_equal 'HTTP/1.1', request.version
+   end
+
+   def test_parses_http_headers_from_simple_get_request
+    request_string = File.read('../get-fruits-with-filter.request.txt')
+     request = Request.new(request_string)
+     hash = {'Host' => 'fruits.com', 'User-Agent' => 'ExampleBrowser/1.0','Accept-Encoding' => 'gzip, deflate','Accept' => '*/*'}
+     assert_equal hash, request.headers
+   end
+
+   def test_parses_http_params_from_simple_get_request
+     request_string = File.read('../get-fruits-with-filter.request.txt')
+     request = Request.new(request_string)
+     hash = {'type' => 'bananas', 'minrating' => '4'}
+     assert_equal hash, request.params
+   end
+
+
+
+  
+
+  
 
 end
